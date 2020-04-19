e4b8a86116d7f9c6a2aafaa776f9434a98c6b50b
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-19 01:19:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+contacts["Freddy Mercury"].each do |key, value|
+  if key == :favorite_ice_cream_flavors && value == "strawberry"
+    key.delete("strawberry")
+  end
+end 
 end
